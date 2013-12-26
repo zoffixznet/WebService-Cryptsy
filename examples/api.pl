c9eed9fc10f9203e5ea6a957eeb9103cd2829b78
Zoffix Znet <cpan@zoffix.com>
2013-12-25 22:10:39 -0500

    Update

--- examples/api.pl
+++ examples/api.pl
@@ -6,8 +6,8 @@ use lib qw(../lib lib);
 
 use WebService::Cryptsy;
 
-my $pub_key = '';
-my $priv_key = '';
+my $pub_key = '479c5cbd116f8f5972bdaf12dd0a3f82562c8a7c';
+my $priv_key = 'b408e899526142ac613304669a657c8782435ccda2f65dbea05270fe8dfa5d3d2ef7eb4812ce1c35';
 
 
 my $cryp = WebService::Cryptsy->new(
